93efa4d03bd3d3b6f3449672ef716d762916f628
Dossymkhan <doszlmnv@gmail.com>
2022-04-06 17:18:50 +0600

    Initial Commit

---
+++ IndividualStudyPlan/IndividualStudyPlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  IndividualStudyPlanApp.swift
+//  IndividualStudyPlan
+//
+//  Created by Dossymkhan Zhulamanov on 06.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct IndividualStudyPlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
